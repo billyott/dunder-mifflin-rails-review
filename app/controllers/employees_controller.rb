4541a4d1697647dd96e34f2f5b9c795c16bf3ef3
billyott <willrichott@gmail.com>
2020-10-08 14:56:08 -0400

    added Employee views + controller methods.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,17 @@
 class EmployeesController < ApplicationController
+    before_action :find_employee, only: [:show, :edit, :update, :destroy]
+
+    def show
+    end
+
+    def index
+        @employees = Employee.all
+    end
+
+    private
+
+    def find_employee
+        @employee = Employee.find(params[:id])
+    end
+
 end
