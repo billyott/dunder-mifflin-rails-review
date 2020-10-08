18e6b8f0ae3f5d8c1524afa1fd27b4937b92d8f1
gooseisdead <gooseisdead@mac.com>
2020-10-08 17:41:32 -0400

    lots more 6pm

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -13,10 +13,20 @@ class EmployeesController < ApplicationController
     end
 
     def update
-        byebug
         @employee.update(employee_params)
         redirect_to employee_path(@employee)
+    end
 
+    def create
+        @employee = Employee.new(employee_params)
+        @employee.save
+        byebug
+        redirect_to employee_path(@employee)
+    end
+
+    def destroy
+        @employee.destroy
+        redirect_to employees_path
     end
 
     private
@@ -26,7 +36,7 @@ class EmployeesController < ApplicationController
     end
 
     def employee_params
-        params.require(:employee).permit(:first_name, :last_name, :title, :alias, :office, :dog, :img_url)
+        params.require(:employee).permit(:first_name, :last_name, :title, :alias, :office, :dog_id, :img_url)
     end
 
 end
