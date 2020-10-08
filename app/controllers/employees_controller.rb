50a3c95b2c7f1b320206072343ee819542694eb9
gooseisdead <gooseisdead@mac.com>
2020-10-08 15:59:57 -0400

    4pm

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -8,10 +8,25 @@ class EmployeesController < ApplicationController
         @employees = Employee.all
     end
 
+    def edit
+
+    end
+
+    def update
+        byebug
+        @employee.update(employee_params)
+        redirect_to employee_path(@employee)
+
+    end
+
     private
 
     def find_employee
         @employee = Employee.find(params[:id])
+    end
+
+    def employee_params
+        params.require(:employee).permit(:first_name, :last_name, :title, :alias, :office, :dog, :img_url)
     end
 
 end
